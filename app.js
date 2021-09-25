03f2ea531e741f460c3e6a984f523ced4b6c4e34
Huma Sahar <humasahar308@gmail.com>
2021-09-26 01:01:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+
+//write a program which shows the couter after each second.
+
+// var seconds =00;
+
+// function incrementSeconds() {
+//     var el = document.getElementById('mySecond').innerText='you are here till '+ seconds + ' second';
+
+//     seconds += 01;
+
+// }
+// var cancel = setInterval(incrementSeconds,1000);
+
+//////////////////////////////////////////////////////////
+
+// write a function which takes your name and displays the greeting with your name
+
+// var userName = prompt('User name','Please write your name');
+
+// alert(`Assalam-o-Alaikum ${userName}, how are you doing today.`)
+
+////////////////////////////////////////////////////////////
+
+
+// write a function(arrow function) which takes two values and return its sum as a result
+
+// add = () => 2+2
+
+// var val = document.getElementById('mySecond').innerHTML = add()
+
+//////////////////////////////////////////////////////////////
+
+
+//write a function(arrow function) which takes a number and multiply it with 0.5 and return 
+//the new value. print the new value outside the function
+
+
+// multiply = () => 2*0.5;
+
+// var result = document.getElementById('mySecond') .innerText = 'The multiply result of 4 x 0.5 is '+ multiply()  
+
+/////////////////////////////////////////////////////////////
+
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+
+
+// const arr = [1, 2, 3, 4, 5]; 
+
+// let x = arr.map((i)=>i++)
+
+// console.log(x)
+
+
+///////////////////////////////////////////////////////////////////
+
+//let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
+
+// let arr = [
+//     {id:0,names:"abc"},
+//     {id:1,names:"efg"},
+//     {id:2,names:"hij"},
+//     {id:3,names:"xyz"}
+// ]
+
+
+
+// var x = arr.map(data);
+// function data(a){
+//     return a.id +" "+ a.names
+// }
+// document.write(x)
